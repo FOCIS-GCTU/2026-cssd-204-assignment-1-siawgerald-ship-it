5e38f07a8ac9dec7f546d95dee52da89741a4b7b
siawgerald-ship-it <siawgerald@gmail.com>
2026-05-30 22:05:12 +0000

    Add files via upload

--- initials.py
+++ initials.py
@@ -1,7 +1,35 @@
+# File: initials.py
+# Description: Print out my initials.
+# Assignment Number: 1
+#
+# Name: GERALD AGYARE-SIAW
+# STUDENT ID: 2425401157
+# Email: SIAWGERALD@GMAIL.COM
+# Grader: AUGUSTUS BUCKMAN
+#
+# On my honor, Gerald Agyare-Siaw, this programming assignment is my own work
+# and I have not provided this code to any other student.
+
 
 def main():
-  pass #code goes here
+    # Print my initials in small and large stylized letters.
 
+    print()
+    print("...GAS")
+    print()
+
+    print("...GGGGGGGGGGGG...AAAAAAAAAAAA...SSSSSSSSSSSS...****")
+    print("...GGGG...........AAAA....AAAA...SSSS...........****")
+    print("...GGGG...........AAAA....AAAA...SSSS...........****")
+    print("...GGGG..GGGGGG...AAAAAAAAAAAA...SSSSSSSSSSSS...****")
+    print("...GGGG....GGGG...AAAA....AAAA...........SSSS...****")
+    print("...GGGG....GGGG...AAAA....AAAA...........SSSS...****")
+    print("...GGGG....GGGG...AAAA....AAAA...........SSSS...****")
+    print("...GGGG....GGGG...AAAA....AAAA...........SSSS...****")
+    print("...GGGGGGGGGGGG...AAAA....AAAA...SSSSSSSSSSSS...****")
+    print("...GGGGGGGGGGGG...AAAA....AAAA...SSSSSSSSSSSS...****")
+
+    print()
 
 
 main()
